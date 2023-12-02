5d64d11456f3f7776c7e5cae9e2eb6e07ee730d7
jesse1412 <jesseserrao@hotmail.co.uk>
2023-12-02 12:19:49 +0000

    Add day 1

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod day1;
+
+fn main() {
+    let day1_part_1 = day1::part1();
+    println!("day 1 part 1: {}", day1_part_1);
+
+    let day1_part_2 = day1::part2();
+    println!("day 1 part 2: {}", day1_part_2);
+}
