b1df7653566053c31cec6ff790a9abf11bd9ea3b
jesse1412 <jesseserrao@hotmail.co.uk>
2023-12-03 21:46:03 +0000

    re-name day1 to day1 2015 (accidentally did wrong year)

---
+++ src/day1_2015.rs
@@ -0,0 +1,26 @@
+pub fn part1() -> i32 {
+    let input = include_str!("inputs/day1_2015.txt");
+    let mut floor: i32 = 0;
+    input.chars().for_each(|c| match c {
+        '(' => floor += 1,
+        ')' => floor -= 1,
+        _ => (),
+    });
+    floor
+}
+
+pub fn part2() -> usize {
+    let input = include_str!("inputs/day1_2015.txt");
+    let mut floor: i32 = 0;
+    for (i, c) in input.chars().enumerate() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => (),
+        };
+        if floor < 0 {
+            return i + 1;
+        }
+    }
+    0
+}
